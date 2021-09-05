9ded208fdb9b8f3520561c84cb638e8b7de8c18e
Leka <zhangtiancrystal@163.com>
2021-09-05 20:43:33 +0800

    第一周作业

---
+++ 1_4.js
@@ -0,0 +1,12 @@
+// 不准使用正则表达式，纯粹用 JavaScript 的逻辑实现：在一个字符串中，找到字符“ab”
+function match(string){
+  let foundA = false;
+  for(let c of string){
+    if(c=='a') foundA = true;
+    else if(foundA && c == 'b') return true;
+    else foundA = false;
+  }
+  return false;
+}
+
+console.log(match('i acbm groot'))
